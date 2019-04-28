398e1e49932b2af8a8d1be6a92ccdb05d80de345
Tamara J. Blätte <tamara.jacqueline@gmail.com>
2019-04-28 13:38:52 +0200

    Add training checkpoint & stats logging for tensorboard

--- network.py
+++ network.py
@@ -5,7 +5,7 @@ import os
 import glob
 
 class Network():
-    def __init__(self, chkpt="training"):  
+    def __init__(self, chkpt_dir="training"):
         # input = map / state of the world
         # output = expected reward for each possible action
         self.model = keras.Sequential([
@@ -23,19 +23,22 @@ class Network():
                 )
 
         # check point saving
-#        checkpoint_path = chkpt + os.path.sep + "cp.ckpt"
-#        checkpoint_dir = os.path.dirname(checkpoint_path)
-#        self.cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, 
-#                                                         save_weights_only=True,
-#                                                         verbose=1)
-#
-#        # if a checkpoint file already exists, load it
-#        if glob.glob(checkpoint_path + "*"):
-#            self.model.load_weights(checkpoint_path)
-#        else:
-#            print("No checkpoint loaded!")
-#            pass
-#
+        checkpoint_path = chkpt_dir + os.path.sep + "cp.ckpt"
+        checkpoint_dir = os.path.dirname(checkpoint_path)
+        self.cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
+                                                         save_weights_only=True,
+                                                         verbose=1)
+        self.tb_callback = tf.keras.callbacks.TensorBoard(log_dir=checkpoint_dir)
+
+
+        # if a checkpoint file already exists, load it
+        if glob.glob(checkpoint_path + "*"):
+            print("Checkpoint loaded!")
+            self.model.load_weights(checkpoint_path)
+        else:
+            print("No checkpoint loaded!")
+            pass
+
     def get_action(self, state):
         # using state, predict snake action rewards and pick the expected optimal action
         print("current state: {}".format(state))
@@ -48,14 +51,14 @@ class Network():
         return self.model.predict(state)
 
 
-    def train(self, states, rewards, epochs=1):
+    def train(self, states, rewards, epochs=10):
         print(states.shape)
         print(rewards.shape)
         self.model.fit(
                 states,
                 rewards,
                 epochs=epochs,
-#                callbacks=[self.cp_callback],
+                callbacks=[self.cp_callback, self.tb_callback],
                 )
 
     def eval(self, states, rewards):
